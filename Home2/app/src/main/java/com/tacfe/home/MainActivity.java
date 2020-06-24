0075a7bda0226527cae10a6dcbac1bf73d7d3f81
tacfe <1021806583@qq.com>
2020-06-24 21:22:11 +0800

    修改首页xml文件名

--- Home2/app/src/main/java/com/tacfe/home/MainActivity.java
+++ Home2/app/src/main/java/com/tacfe/home/MainActivity.java
@@ -3,18 +3,22 @@ package com.tacfe.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button bt_1;
     private Button bt_2;
     private Button bt_3;
+    private EditText HomeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
         bt_1 = findViewById(R.id.bt_1);
         bt_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +40,23 @@ public class MainActivity extends AppCompatActivity {
                 // 跳转到个人中心
             }
         });
+        HomeText = findViewById(R.id.ed_1);
+        HomeText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 }
