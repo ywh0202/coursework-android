3014b596d7641737f6bbc988913c083ab80731b2
tyler <44457606+duanfuqiang1128@users.noreply.github.com>
2020-06-24 22:23:22 +0800

    update StartActivity

--- app/src/main/java/com/example/sign_in/StartActivity.java
+++ app/src/main/java/com/example/sign_in/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.sign_in;
 
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+        SharedPreferences config = getSharedPreferences("config", MODE_PRIVATE);
+        String token = config.getString("token", "");
+        if (token.isEmpty()){
+            Log.d("StartActivate", "token is empty");
+        }
+        Log.d("StartActivate", token);
     }
 }
