2c2f1cc3632a9b38ca4eb5eac617e2a6e7148220
baozhongle <2675971784@qq.com>
2020-07-02 00:29:08 +0800

    最终合并

---
+++ app/src/main/java/com/example/sign_in/UserData.java
@@ -0,0 +1,45 @@
+package com.example.sign_in;
+
+public class UserData {
+
+    private Payloadsss payload;
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public Payloadsss getPayload() {
+
+        return payload;
+    }
+}
+
+
+class Payloadsss{
+
+private String user_name;
+private String gender;
+private String user_account;
+private  String passwd;
+
+public String getUser_name() {
+
+        return user_name;
+        }
+
+
+public String getGender() {
+
+        return gender;
+        }
+
+public String getUser_account() {
+
+        return user_account;
+        }
+public  String getPasswd() {
+
+        return passwd;
+    }
+}
